bb9e3def84d238c1a2b091ef2037811ef6d5e98e
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2026-04-24 21:49:37 +0900

    modify README and production.rb

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # ログ出力にリクエストIDを追加
+  config.log_tags = [ :request_id ]
 end
